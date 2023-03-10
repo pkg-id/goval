e6e476b578ebb2e3f130c65abc1cbc05b25ace71
Jose Sitanggang <josealfredositanggang@gmail.com>
2023-03-10 16:36:09 +0700

    feat: add map validator

--- maps.go
+++ maps.go
@@ -1,1 +1,56 @@
 package goval
+
+import "context"
+
+type MapValidator[K comparable, V any] func(ctx context.Context, values map[K]V) error
+
+func Map[K comparable, V any]() MapValidator[K, V] {
+	return NopValueValidator[map[K]V]
+}
+
+// Build attaches the value so the rules chain can consume it as an input that need to be validated.
+func (mv MapValidator[K, V]) Build(values map[K]V) Validator {
+	return validatorOf(mv, values)
+}
+
+// Required ensures the length is not 0 or the map is not nil.
+func (mv MapValidator[K, V]) Required() MapValidator[K, V] {
+	return Chain(mv, func(ctx context.Context, values map[K]V) error {
+		if values == nil || len(values) == 0 {
+			return Errorf("is required")
+		}
+
+		return nil
+	})
+}
+
+// Min ensures the length is not less than the given min.
+func (mv MapValidator[K, V]) Min(min int) MapValidator[K, V] {
+	return Chain(mv, func(ctx context.Context, values map[K]V) error {
+		if len(values) < min {
+			return Errorf("length must be greater than %v", min)
+		}
+		return nil
+	})
+}
+
+// Max ensures the length is not greater than the given max.
+func (mv MapValidator[K, V]) Max(max int) MapValidator[K, V] {
+	return Chain(mv, func(ctx context.Context, values map[K]V) error {
+		if len(values) > max {
+			return Errorf("length must be less than %v", max)
+		}
+		return nil
+	})
+}
+
+func (mv MapValidator[K, V]) Each(builder Builder[V]) MapValidator[K, V] {
+	return func(ctx context.Context, values map[K]V) error {
+		for key, value := range values {
+			if err := builder.Build(value).Validate(ctx); err != nil {
+				return Errorf("key:%v, got: %s", key, err)
+			}
+		}
+		return nil
+	}
+}
--- maps_test.go
+++ maps_test.go
@@ -1,1 +1,65 @@
-package goval
+package goval_test
+
+import (
+	"context"
+	"github.com/pkg-id/goval"
+	"testing"
+)
+
+func TestMapValidator_Required(t *testing.T) {
+	err := goval.Map[string, int]().Required().Build(nil).Validate(context.Background())
+	if err == nil {
+		t.Errorf("expect got an error: %v", err)
+	}
+
+	err = goval.Map[string, int]().Required().Build(map[string]int{}).Validate(context.Background())
+	if err == nil {
+		t.Errorf("expect got an error: %v", err)
+	}
+
+	err = goval.Map[string, int]().Required().Build(map[string]int{"a": 1, "b": 2}).Validate(context.Background())
+	if err != nil {
+		t.Errorf("expect no error")
+	}
+}
+
+func TestMapValidator_Min(t *testing.T) {
+	err := goval.Map[string, int]().Required().Min(3).Build(map[string]int{"a": 1, "b": 2}).Validate(context.Background())
+	if err == nil {
+		t.Errorf("expect got an error: %v", err)
+	}
+
+	err = goval.Map[string, int]().Required().Min(1).Build(map[string]int{"a": 1, "b": 2}).Validate(context.Background())
+	if err != nil {
+		t.Errorf("expect no error")
+	}
+}
+
+func TestMapValidator_Max(t *testing.T) {
+	err := goval.Map[string, int]().Required().Min(1).Max(1).Build(map[string]int{"a": 1, "b": 2}).Validate(context.Background())
+	if err == nil {
+		t.Errorf("expect got an error: %v", err)
+	}
+
+	err = goval.Map[string, int]().Required().Min(1).Max(10).Build(map[string]int{"a": 1, "b": 2}).Validate(context.Background())
+	if err != nil {
+		t.Errorf("expect no error")
+	}
+}
+
+func TestMapValidator_Each(t *testing.T) {
+	err := goval.Map[string, int]().Required().Min(1).Max(3).
+		Each(goval.Number[int]().Min(4)).
+		Build(map[string]int{"a": 1, "b": 2}).Validate(context.Background())
+	if err == nil {
+		t.Errorf("expect got an error: %v", err)
+	}
+
+	err = goval.Map[string, int]().Required().Min(1).Max(3).
+		Each(goval.Number[int]().Min(4)).
+		Build(map[string]int{"a": 10, "b": 20}).Validate(context.Background())
+
+	if err != nil {
+		t.Errorf("expect no error")
+	}
+}
