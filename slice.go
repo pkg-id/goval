f1b21b5bc09367d369e5b41277bbd30bb7acfec8
Jose Sitanggang <josealfredositanggang@gmail.com>
2023-03-10 16:23:13 +0700

    feat: add slice validator

--- slice.go
+++ slice.go
@@ -1,1 +1,56 @@
 package goval
+
+import "context"
+
+type SliceValidator[T any, V []T] func(ctx context.Context, values V) error
+
+func Slice[T any, V []T]() SliceValidator[T, V] {
+	return NopValueValidator[V]
+}
+
+// Build attaches the value so the rules chain can consume it as an input that need to be validated.
+func (nv SliceValidator[T, V]) Build(values V) Validator {
+	return validatorOf(nv, values)
+}
+
+// Required ensures the number is not a zero value.
+func (nv SliceValidator[T, V]) Required() SliceValidator[T, V] {
+	return Chain(nv, func(ctx context.Context, values V) error {
+		if values == nil || len(values) == 0 {
+			return Errorf("is required")
+		}
+
+		return nil
+	})
+}
+
+// Min ensures the number is not less than the given min.
+func (nv SliceValidator[T, V]) Min(min int) SliceValidator[T, V] {
+	return Chain(nv, func(ctx context.Context, values V) error {
+		if len(values) < min {
+			return Errorf("length must be greater than %v", min)
+		}
+		return nil
+	})
+}
+
+// Max ensures the number is not greater than the given max.
+func (nv SliceValidator[T, V]) Max(max int) SliceValidator[T, V] {
+	return Chain(nv, func(ctx context.Context, values V) error {
+		if len(values) > max {
+			return Errorf("length must be less than %v", max)
+		}
+		return nil
+	})
+}
+
+func (nv SliceValidator[T, V]) Each(builder Builder[T]) SliceValidator[T, V] {
+	return func(ctx context.Context, values V) error {
+		for index, value := range values {
+			if err := builder.Build(value).Validate(ctx); err != nil {
+				return Errorf("index:%d, got: %s", index, err)
+			}
+		}
+		return nil
+	}
+}
--- slice_test.go
+++ slice_test.go
@@ -1,1 +1,80 @@
-package goval
+package goval_test
+
+import (
+	"context"
+	"github.com/pkg-id/goval"
+	"testing"
+)
+
+func TestSliceValidator_Required(t *testing.T) {
+	err := goval.Slice[int]().Required().Build(nil).Validate(context.Background())
+	if err == nil {
+		t.Errorf("expect got an error: %v", err)
+	}
+
+	err = goval.Slice[int]().Required().Build([]int{}).Validate(context.Background())
+	if err == nil {
+		t.Errorf("expect got an error: %v", err)
+	}
+
+	err = goval.Slice[int]().Required().Build([]int{1, 2}).Validate(context.Background())
+	if err != nil {
+		t.Errorf("expect no error")
+	}
+
+	err = goval.Slice[string]().Required().Build(nil).Validate(context.Background())
+	if err == nil {
+		t.Errorf("expect got an error: %v", err)
+	}
+
+	err = goval.Slice[string]().Required().Build([]string{}).Validate(context.Background())
+	if err == nil {
+		t.Errorf("expect got an error: %v", err)
+	}
+
+	err = goval.Slice[string]().Required().Build([]string{"a", "b"}).Validate(context.Background())
+	if err != nil {
+		t.Errorf("expect no error")
+	}
+}
+
+func TestSliceValidator_Min(t *testing.T) {
+	err := goval.Slice[int]().Required().Min(3).Build([]int{1, 2}).Validate(context.Background())
+	if err == nil {
+		t.Errorf("expect got an error: %v", err)
+	}
+
+	err = goval.Slice[int]().Required().Min(1).Build([]int{1, 2}).Validate(context.Background())
+	if err != nil {
+		t.Errorf("expect no error")
+	}
+}
+
+func TestSliceValidator_Max(t *testing.T) {
+	err := goval.Slice[int]().Required().Min(1).Max(1).Build([]int{1, 2}).Validate(context.Background())
+	if err == nil {
+		t.Errorf("expect got an error: %v", err)
+	}
+
+	err = goval.Slice[int]().Required().Min(1).Max(10).Build([]int{1, 2}).Validate(context.Background())
+	if err != nil {
+		t.Errorf("expect no error")
+	}
+}
+
+func TestSliceValidator_Each(t *testing.T) {
+	err := goval.Slice[int]().Required().Min(1).Max(3).
+		Each(goval.Number[int]().Min(4)).
+		Build([]int{1, 2}).Validate(context.Background())
+	if err == nil {
+		t.Errorf("expect got an error: %v", err)
+	}
+
+	err = goval.Slice[int]().Required().Min(1).Max(3).
+		Each(goval.Number[int]().Min(4)).
+		Build([]int{10, 20}).Validate(context.Background())
+
+	if err != nil {
+		t.Errorf("expect no error")
+	}
+}
