adfff63a61bb806f7c22296b4a3eed4982e3f9cd
Jose Sitanggang <josealfredositanggang@gmail.com>
2023-03-13 19:18:07 +0700

    refactor(PtrValidator): rename NilValidator to PtrValidator.

--- ptr.go
+++ ptr.go
@@ -2,15 +2,15 @@ package goval
 
 import "context"
 
-type NilValidator[T any] FunctionValidator[*T]
+type PtrValidator[T any] FunctionValidator[*T]
 
-func Nil[T any]() NilValidator[T] { return NopValueValidator[*T] }
+func Ptr[T any]() PtrValidator[T] { return NopValueValidator[*T] }
 
-func (f NilValidator[T]) Build(value *T) Validator {
+func (f PtrValidator[T]) Build(value *T) Validator {
 	return validatorOf(f, value)
 }
 
-func (f NilValidator[T]) Required() NilValidator[T] {
+func (f PtrValidator[T]) Required() PtrValidator[T] {
 	return Chain(f, func(ctx context.Context, value *T) error {
 		if value == nil {
 			return NewRuleError(NilRequired, value)
@@ -19,7 +19,7 @@ func (f NilValidator[T]) Required() NilValidator[T] {
 	})
 }
 
-func (f NilValidator[T]) Optional(builder Builder[T]) NilValidator[T] {
+func (f PtrValidator[T]) Optional(builder Builder[T]) PtrValidator[T] {
 	return Chain(f, func(ctx context.Context, value *T) error {
 		if value != nil {
 			return builder.Build(*value).Validate(ctx)
@@ -28,7 +28,7 @@ func (f NilValidator[T]) Optional(builder Builder[T]) NilValidator[T] {
 	})
 }
 
-func (f NilValidator[T]) Next(builder Builder[T]) NilValidator[T] {
+func (f PtrValidator[T]) Next(builder Builder[T]) PtrValidator[T] {
 	return Chain(f, func(ctx context.Context, value *T) error {
 		return builder.Build(*value).Validate(ctx)
 	})
