43ab33cdbfbd6195ca2f9980bceee32271dbda38
Jose Sitanggang <josealfredositanggang@gmail.com>
2023-03-29 00:42:15 +0700

    docs: add go doc for Contains function.

--- funcs/funcs.go
+++ funcs/funcs.go
@@ -1,5 +1,6 @@
 package funcs
 
+// Contains returns true if one of the given values satisfy the predicate.
 func Contains[T comparable, P func(value T) bool](values []T, predicate P) bool {
 	for i := range values {
 		if predicate(values[i]) {
